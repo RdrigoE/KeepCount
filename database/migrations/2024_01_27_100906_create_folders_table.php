b7cac948dbb9d3f357a72cf7955f5b2355301bd0
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2024-01-27 10:15:50 +0000

    create folder and folders migration

---
+++ database/migrations/2024_01_27_100906_create_folders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Folder;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('folders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignIdFor(Folder::class, 'folder_id')
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate()
+                ->nullable();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('folders');
+    }
+};
